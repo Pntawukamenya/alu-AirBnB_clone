fe024959a19d23f24f023d83e343c80c9008f255
Stella-Remember <s.habiyambe@alustudent.com>
2024-05-18 18:44:55 +0000

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,25 +28,20 @@ class FileStorage():
         self.__objects[key] = obj
 
     def save(self):
-        from models.base_model import BaseModel
-        """Serializes __objects to the JSON file"""
-        data = {}
-        for key, obj in self.__objects.items():
-            data[key] = obj.to_dict()
-        with open(self.__file_path, 'w') as file:
-            json.dump(data, file)
+        """Serialize __objects to the JSON file __file_path."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
 
     def reload(self):
-        from models.base_model import BaseModel
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(self.__file_path, 'r') as file:
-                data = json.load(file)
-                for key, obj_dict in data.items():
-                    class_name, obj_id = key.split('.')
-                    obj = eval(class_name)(**obj_dict)
-                    self.__objects[key] = obj
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
         except FileNotFoundError:
-            pass
-
-storage = FileStorage()
-storage.reload()
+            return
